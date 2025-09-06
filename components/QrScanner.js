08b6f3c3b9c61b22a0ab150d05b3fa904f4e2ccb
PT GIMA HOSTING INDONESIA <yohanesmatius509@gmail.com>
2025-09-07 00:54:08 +0800

    Update QrScanner.js

--- components/QrScanner.js
+++ components/QrScanner.js
@@ -1,5 +1,18 @@
 import { useEffect, useRef } from 'react';
 import jsQR from 'jsqr';
+import { QrReader } from 'react-qr-reader3';
+
+export default function QrScanner({ onScan }) {
+  return (
+    <QrReader
+      constraints={{ facingMode: 'environment' }}
+      onResult={(result, error) => {
+        if (result) onScan(result?.text);
+      }}
+      style={{ width: '100%' }}
+    />
+  );
+}
 
 export default function QrScanner({ onScan }) {
   const videoRef = useRef(null);
