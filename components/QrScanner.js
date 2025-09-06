6f6071e0fa7c85b3a093adba0625cbbf57db90fc
PT GIMA HOSTING INDONESIA <yohanesmatius509@gmail.com>
2025-09-07 00:41:32 +0800

    Create QrScanner.js

---
+++ components/QrScanner.js
@@ -0,0 +1,43 @@
+import { useEffect, useRef } from 'react';
+import jsQR from 'jsqr';
+
+export default function QrScanner({ onScan }) {
+  const videoRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const startCamera = async () => {
+      const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } });
+      videoRef.current.srcObject = stream;
+      videoRef.current.play();
+      tick();
+    };
+
+    const tick = () => {
+      const video = videoRef.current;
+      const canvas = canvasRef.current;
+      const ctx = canvas.getContext('2d');
+      if (video.readyState === video.HAVE_ENOUGH_DATA) {
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
+        ctx.drawImage(video, 0, 0);
+        const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        const code = jsQR(imageData.data, canvas.width, canvas.height);
+        if (code) {
+          onScan(code.data);
+          return;
+        }
+      }
+      requestAnimationFrame(tick);
+    };
+
+    startCamera();
+  }, []);
+
+  return (
+    <>
+      <video ref={videoRef} style={{ width: '100%' }} />
+      <canvas ref={canvasRef} hidden />
+    </>
+  );
+}
