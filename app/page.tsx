e529e6382719decf67e977f33672095e7742f0e1
PT GIMA HOSTING INDONESIA <yohanesmatius509@gmail.com>
2025-09-07 21:45:46 +0800

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="p-6">
+      <h1 className="text-2xl font-bold">Absensi Sekolah</h1>
+      <a href="/scan" className="text-blue-600 underline">Buka Scanner</a>
+    </main>
+  );
+}
